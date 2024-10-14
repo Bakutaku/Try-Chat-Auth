52a895e7788efda69ef58bbdcafe1caaad31f56d
Bakutaku <133964557+Bakutaku@users.noreply.github.com>
2024-10-14 14:21:28 +0900

    Hello World
    はじめのコミット

---
+++ src/main/java/com/bakutaku/try_chat_auth/TryChatAuthApplication.java
@@ -0,0 +1,13 @@
+package com.bakutaku.try_chat_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryChatAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryChatAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bakutaku/try_chat_auth/TryChatAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakutaku.try_chat_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryChatAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
